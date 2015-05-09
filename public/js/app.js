8faccab47e3c8e5ce6b09b149e64a358c273ce8d
Tyler Lubeck <tyler@tylerlubeck.com>
2015-05-09 15:31:52 -0400

    Add filtering

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,24 @@
+$(document).ready(function() {
+    $('.modal-trigger').leanModal();
+});
 $(function() {
     var mapOptions = {
         center: { lat: 42.3601, lng: -71.0589 },
-        zoom: 5
+        zoom: 5,
+        disableDefaultUI: true,
+        panControl: true,
+        zoomControl: true,
+        
     };
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
     var spiderfier = prepSpiderfier(map);
     var openWindow = null;
+    var $typesFilter = $('#types-dropdown');
+    var $companyFilter = $('#company-dropdown');
+    var jobType = null;
+    var companyName = null;
+    var allAlumni = null;
+    var $currentlyShowing = $('#currentlyShowing');
 
     $.get('/api/alumni', gotAlumni);
     
@@ -39,19 +52,116 @@ $(function() {
 
     /*
      * Plot of the alumni on the map, and prepare their infowindows
-     * @param {Object} alumni - The alumni to plot
+     * @param {Array} alumni - The alumni to plot
      *
      */
     function gotAlumni(alumni) {
+        allAlumni = alumni;
+        var types = {};
+        var companies = {};
         alumni.forEach(function renderAlumn(alumn) {
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(alumn.lat, alumn.lng),
-                map: map,
-                title: alumn.name,
-                alumn: alumn
-            });
-            spiderfier.addMarker(marker);
+            if ( alumn.type != undefined ) {
+                types[alumn.type] = true;
+            }
+            if (alumn.company != undefined) {
+                companies[alumn.company] = true;
+            }
+            addMarkerToMap(alumn);
         });
+        var jobTypes = Object.keys(types);
+        prepFilters(jobTypes, Object.keys(companies));
+    }
+
+    function addMarkerToMap(alumn) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(alumn.lat, alumn.lng),
+            map: map,
+            title: alumn.name,
+            alumn: alumn
+        });
+        spiderfier.addMarker(marker);
+    }
+
+    function showAlumni(alumni) {
+        closeInfoWindow(openWindow);
+        console.log("Filtering for: ", jobType, companyName);
+        var msg = 'Currently showing all alumni';
+        if (jobType !== null && companyName !== null) {
+            msg = 'Currently filtering for ' + jobType + ' positions at ' + companyName;
+        } else if ( companyName !== null ) {
+            msg = 'Currently filtering for all job types at ' + companyName;
+        } else if ( jobType !== null ) {
+            msg = 'Currently filtering for ' + jobType + ' at all companies';
+        }
+
+        $currentlyShowing.text(msg);
+        spiderfier.getMarkers().forEach(function (marker) {
+            marker.setMap(null);
+        });
+        spiderfier.clearMarkers();
+
+        alumni.filter(function(alumn) {
+            // If we're not currently filtering, accept all
+            if ( jobType === null && companyName === null ) {
+                return true;
+            }
+
+            // If company is correct and we're not filtering by job type
+            if ( jobType === null && alumn.company === companyName) {
+                return true;
+            }
+
+            // If job type is correct and we're not filtering by company
+            if ( companyName === null && alumn.type === jobType ) {
+                return true;
+            }
+
+            // If both company and job type are the same
+            if ( companyName === alumn.company && alumn.type === jobType ) {
+                return true
+            }
+
+            return false;
+
+        }).forEach(function(alumn) {
+            addMarkerToMap(alumn);
+        });
+    }
+
+    function prepFilters(jobTypes, companies) {
+        for(var i = 0; i < jobTypes.length; ++i) {
+            var type = jobTypes[i];
+            $typesFilter.append($('<li><a href="#!" class="typeFilter">' + type + '</a></li>'));
+        }
+
+        for(var i = 0; i < companies.length; ++i) {
+            var company = companies[i];
+            $companyFilter.append($('<li><a href="#!" class="companyFilter">' + company + '</a></li>'));
+        }
+
+
+        $('.typeFilter').click(function(e) {
+            var msg = this.text;
+            console.log('Setting jobType to', msg);
+            if (msg === 'Show All') {
+                jobType = null;
+            } else {
+                jobType = this.text;
+            }
+            showAlumni(allAlumni);
+        });
+
+        $('.companyFilter').click(function(e) {
+            var msg = this.text;
+            console.log('Setting companyName to', msg);
+            if (msg === 'Show All') {
+                companyName = null;
+            } else {
+                companyName = this.text;
+            }
+            showAlumni(allAlumni);
+        });
+
     }
 
     /* 
